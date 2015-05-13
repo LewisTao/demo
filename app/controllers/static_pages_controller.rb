63e1064ebf1964cfb3b49d3b732b4d18efecde88
LewisTao <huytaoquang@gmail.com>
2015-05-13 22:41:07 +0700

    [STATIC PAGES] setup basic static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+	def home_page
+	end
+
+	def about_page
+	end
+
+	def FAQ_page
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: false) do
-    end
-  end
-  end
+	# rails admin
+  	mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  	# routes setup
+  	root 'static_pages#home_page'
+	get '/about', to: 'static_pages#about_page'
+  	get '/FAQ', to: 'static_pages#FAQ_page'
+  
+  	# api setup
+  	namespace :api, constraints: {subdomain: 'api'}, path: '/' do
+    	scope module: :v1, constraints: ApiConstraints.new(version: 1, default: false) do
+    	end
+	end
+end
